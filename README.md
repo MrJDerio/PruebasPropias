ef00e16afc3cb046521f177bf27097f7fe82c710
java <java@ipartekaula.com>
2018-03-15 13:54:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PruebasAjenas
+# PruebasAjenas
---
+++ primerBoot/src/main/java/com/ipartek/primerBoot/beans/Persona.java
@@ -0,0 +1,55 @@
+package com.ipartek.primerBoot.beans;
+
+public class Persona {
+
+	private int id;
+	private String nombre;
+	private String apodo;
+	private Pais pais;
+	private Ciudad ciudad;
+	
+//	public Persona() {
+//		
+//	}
+//	
+//	public Persona(int id, String nombre, String apodo, Pais pais) {
+//		super();
+//		this.id = id;
+//		this.nombre = nombre;
+//		this.apodo = apodo;
+//		this.pais = pais;
+//	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApodo() {
+		return apodo;
+	}
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+	
+	
+}
